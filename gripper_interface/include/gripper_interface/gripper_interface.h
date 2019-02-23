9add1f956e450b07569a18313ccb789d31b136a2
Surag Balajepalli <surag.krishna@gmail.com>
2019-02-23 13:23:51 -0500

    successful with gripper open loop tests

--- gripper_interface/include/gripper_interface/gripper_interface.h
+++ gripper_interface/include/gripper_interface/gripper_interface.h
@@ -13,17 +13,28 @@ class GripperInterface
 {
 public:
 	GripperInterface();
-	bool isStalled();
+	bool graspObject(std::string, double);
 	bool graspObject(std::string);
-	bool graspObject();
-	bool releaseObject(std::string);
+	bool graspObject(); 
+	bool releaseObject(std::string, double); 
+	bool releaseObject(double);
 	bool releaseObject();
+
 private:
-	std::map<std::string,double> part_width_mappings_;
-	const double MAX_EFFORT_ = 1.0, MAX_OPEN_ = 0.1, OPEN_EFFORT_ = 1.0, DEFAULT_GRASP_WIDTH_ = 0.1 ;
+	std::map<std::string,double> part_width_map_ = 
+	{
+		{"dummy_part", 0.01}
+	}; //more parts go here as we find out about them
+
+	const double MAX_EFFORT_ = 1.0, OPEN_POSITION_ = 0.1, OPEN_EFFORT_ = 1.0, DEFAULT_GRASP_WIDTH_ = 0.1 ;
+	
 	actionlib::SimpleActionClient<control_msgs::GripperCommandAction> ac_;
 	control_msgs::GripperCommandGoal goal_;
 	control_msgs::GripperCommandResult result_;
+	
+	bool waitForGrasp(double timeout);
+	bool isGrasping();
+	bool waitForRelease(double timeout);
 
 
 };
