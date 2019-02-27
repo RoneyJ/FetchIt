5141d50a612176af224dc8f787fe6f4bbefcd966
bruceleu <mxl592@case.edu>
2019-02-27 16:37:11 -0500

    update

--- perception/fetch_vision/src/box_filter_and_segment.cpp
+++ perception/fetch_vision/src/box_filter_and_segment.cpp
@@ -52,7 +52,10 @@ const float MAX_X = 0.9; //include points out to 0.9m in front of robot
 const float MIN_Y = -0.5; //include points starting -0.5m to left of robot
 const float MAX_Y = 0.5; //include points up to 0.5m to right of robot
 const float MIN_Z = -0.05; //2cm above the table top
-const float MAX_Z = 0.05; //consider points up to 0.12m above table top
+const float MAX_Z = 0.1; //consider points up to this height w/rt torso frame
+
+const float TABLE_TOP_MIN = -0.1;
+const float TABLE_TOP_MAX = 0.05;
 
 //choose, e.g., resolution of 5mm, so 100x200 image for 0.5m x 1.0m pointcloud
 // adjustable--> image  size
@@ -65,6 +68,11 @@ const int Nv = (int) ((MAX_Y - MIN_Y) * PIXELS_PER_METER);
 Mat_<uchar> bw_img(Nu, Nv);
 Mat_<int>labelImage(Nu, Nv);
 Mat dst(bw_img.size(), CV_8UC3);
+
+vector<float> g_x_centroids,g_y_centroids;
+vector<float> g_x_centroids_wrt_robot,g_y_centroids_wrt_robot;
+vector<float> g_avg_z_heights;
+vector<float> g_npts_blobs;
 
 
 void find_indices_box_filtered(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud_ptr, Eigen::Vector3f box_pt_min,
@@ -154,9 +162,24 @@ Eigen::Affine3f compute_affine_cam_wrt_torso_lift_link(void) {
 void blob_color(void) {
 
     //openCV function to do all the  hard work
-    int nLabels = connectedComponents(bw_img, labelImage, 4); //4 vs 8 connected regions
-
+    int nLabels = connectedComponents(bw_img, labelImage, 8); //4 vs 8 connected regions
+    ROS_INFO("found %d blobs",nLabels);
+    g_x_centroids.resize(nLabels);
+    g_y_centroids.resize(nLabels);
+    g_x_centroids_wrt_robot.resize(nLabels);
+    g_y_centroids_wrt_robot.resize(nLabels);
+    g_avg_z_heights.resize(nLabels);
+    g_npts_blobs.resize(nLabels);
+    for (int label = 0; label < nLabels; ++label) {
+        g_x_centroids[label]=0.0;
+        g_y_centroids[label]=0.0;
+        g_x_centroids_wrt_robot[label]=0.0;
+        g_y_centroids_wrt_robot[label]=0.0;
+        g_avg_z_heights[label]=0.0;
+        g_npts_blobs[label]=0.0;
+    }
     //colorize the regions and display them:
+    //also compute centroids;
     std::vector<Vec3b> colors(nLabels);
     colors[0] = Vec3b(0, 0, 0); //make the background black
     //assign random color to each region label
@@ -170,8 +193,28 @@ void blob_color(void) {
             int label = labelImage.at<int>(r, c);
             Vec3b &pixel = dst.at<Vec3b>(r, c);
             pixel = colors[label];
+            //while  we're combing through pixels, compute centroids as well:
+            g_y_centroids[label]+=c;
+            g_x_centroids[label]+=r;
+            g_npts_blobs[label]+=1.0;
         }
     }
+    ROS_INFO("image size: %d cols, %d rows",dst.cols,dst.rows);
+           
+    for (int label = 0; label < nLabels; ++label) {
+        g_x_centroids[label]/=g_npts_blobs[label];
+        g_y_centroids[label]/=g_npts_blobs[label];
+
+        ROS_INFO("label %d has %f points w/ centroid %f, %f:",label,g_npts_blobs[label],g_x_centroids[label],g_y_centroids[label]);
+    }
+    //convert to robot coords:
+    //convert to dpixel_x, dpixel_y w/rt image centroid, scale by pixels/m, and add offset from PCL cropping, x,y
+    for (int label = 1; label < nLabels; ++label) {    
+        g_x_centroids_wrt_robot[label] = ((dst.rows/2) - g_x_centroids[label])/PIXELS_PER_METER + (MIN_X+MAX_X)/2.0;
+        g_y_centroids_wrt_robot[label] = (g_y_centroids[label]- (dst.cols/2))/PIXELS_PER_METER + (MIN_Y+MAX_Y)/2.0;
+        ROS_INFO("label %d has centroid w/rt robot: %f, %f:",label,g_x_centroids_wrt_robot[label],g_y_centroids_wrt_robot[label]);
+                
+    }    
     //display the result in an openCV window
     //imshow("Connected Components", dst); //do this  from "main" instead
 }
@@ -237,12 +280,12 @@ int main(int argc, char** argv) {
 
     //don't need this any more; already found the table height
     // but run it just for fun; remove later for better speed
-    double table_height = find_table_height(transformed_cloud_ptr, -2.0, 2.0, 0.01);
+    double table_height = find_table_height(transformed_cloud_ptr, TABLE_TOP_MIN, TABLE_TOP_MAX, 0.01);
 
     //specify opposite corners of a box to box-filter the transformed points
     //magic numbers are at top of this program
     Eigen::Vector3f box_pt_min, box_pt_max;
-    box_pt_min << MIN_X, MIN_Y, MIN_Z;
+    box_pt_min << MIN_X, MIN_Y, table_height+0.01; //1cm above table top
     box_pt_max << MAX_X, MAX_Y, MAX_Z;
 
     //find which points in the transformed cloud are within the specified box
@@ -289,6 +332,8 @@ int main(int argc, char** argv) {
     //find connected components; 
     //operates on global bw_img and puts region-labeled codes in global Mat "labelImage" 
     blob_color();
+    
+    
 
     //create openCV display windows
     // this is only for debugging; can go away later
---
+++ teamCase_fetch_model/README.md
@@ -0,0 +1,7 @@
+# teamCase_fetch_model
+
+modified fetch model to include stickyfingers plugin on right finger link
+to launch and use this model:
+
+`roslaunch worlds Fetch_kit.launch`
+    
